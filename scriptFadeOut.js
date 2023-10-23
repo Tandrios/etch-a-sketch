fdafa834e8a30fd03ed9b856b8cc68534cf6c696
Tandrios <t.andriessen@gmail.com>
2023-10-23 22:40:10 +0200

    Rename JS files and default Darken

--- scriptFadeOut.js
+++ scriptFadeOut.js
@@ -1,9 +1,29 @@
-//RGB Fade out version
-
-const GRIDSIZE = 50;
+//Darker version
 const DOCUMENTWIDTH = 600;
-const DIVSIZE = DOCUMENTWIDTH / GRIDSIZE;
 const TRANSITIONTIME = 5000
+
+document.addEventListener('DOMContentLoaded' , () => {
+    btn = document.querySelector('button').addEventListener('click', () => {
+        let gridsize = prompt("What is the width of the grid? (max 100)");
+        if (gridsize > 100) {
+            gridsize = 100;
+        }
+        createGrid(gridsize)
+    })
+
+    createGrid(30);
+    
+})
+
+function createGrid(gridsize) {
+    const grid = document.querySelector('.grid-container');
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild)
+    }
+    for (let i = 0; i < (gridsize * gridsize); i++) {
+        createGridbox(gridsize);
+    }
+}
 
 function createColor() {
     const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
@@ -16,11 +36,19 @@ function createColor() {
     return rgb
 }
 
-function createGridbox() {
+function darkenColor(color) {
+    let newColor = parseInt(color * 0.8);
+    if (newColor > 255) {
+        return 255
+    }
+    return newColor
+}
+
+function createGridbox(gridsize) {
     const container = document.querySelector('.grid-container')
     const div = document.createElement('div')
 
-    div.style.cssText = `width: ${DIVSIZE}px; height: ${DIVSIZE}px`
+    div.style.cssText = `width: ${DOCUMENTWIDTH / gridsize}px; height: ${DOCUMENTWIDTH / gridsize}px`
     div.classList = 'grid';
 
     div.addEventListener('mouseenter', (e) => {
@@ -37,12 +65,8 @@ function createGridbox() {
         div.style.removeProperty("background");
         div.classList.add("grid-mouseLeave");
     });
+    
     container.appendChild(div)
 
 }
 
-document.addEventListener('DOMContentLoaded' , () => {
-    for (let i = 0; i < (GRIDSIZE * GRIDSIZE); i++) {
-        createGridbox();
-    } 
-})
